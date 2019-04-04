e155d4fcd888e9e96abc040cbc0ca726019048cd
cmerwich <christiaan.erwich@gmail.com>
2019-04-04 14:25:45 +0200

    testing mention maker

---
+++ test/tf/2017/otype.tf
@@ -0,0 +1,21 @@
+@node
+@moduleAuthor=Christiaan M. Erwich
+@moduleReason=add a new node type for mentions
+@valueType=str
+@writtenBy=Text-Fabric
+@dateWritten=2019-04-04T11:40:03Z
+
+1-426584	word
+426585-426623	book
+426624-427552	chapter
+427553-515653	clause
+515654-606322	clause_atom
+606323-651502	half_verse
+651503-904689	phrase
+904690-1172208	phrase_atom
+1172209-1235919	sentence
+1235920-1300405	sentence_atom
+1300406-1414189	subphrase
+1414190-1437402	verse
+1437403-1446635	lex
+1446636-1770729	mention
